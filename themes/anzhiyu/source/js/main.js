d10f4359d8c35a79084a52d8491486e44c83bb45
itousouta15 <kuoray333@gmail.com>
2025-10-05 17:17:37 +0800

    feat: 修正翻譯問題

--- themes/anzhiyu/source/js/main.js
+++ themes/anzhiyu/source/js/main.js
@@ -1475,7 +1475,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // 开发者工具键盘监听
   window.onkeydown = function (e) {
-    123 === e.keyCode && anzhiyu.snackbarShow("开发者模式已打开，请遵循GPL协议", !1);
+    123 === e.keyCode && anzhiyu.snackbarShow("開發者模式已打開，請遵循 GPL 協議", !1);
   };
 
   // 欢迎语
